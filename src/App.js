cbf290479caa0cfe3b8d8ce1312ef95a9934e4c2
7nib <7nirbhay@gmail.com>
2023-07-30 06:44:15 -0700

    browser skelton ......

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-
+import {BrowserRouter, Navigate, Routes, Route} from 'react-router-dom';
+import HomePage from 'screens/HomePage';
+import LoginPage from 'screens/LoginPage';
+import ProfilePage from 'screens/ProfilePage/ProfilePage';
 
 function App() {
   return (
